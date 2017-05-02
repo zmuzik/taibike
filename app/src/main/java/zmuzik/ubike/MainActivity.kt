a68eca23d4456f661a335012fb09cdde1d09b3c9
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-05-02 12:34:31 +0800

    kotlinize

---
+++ app/src/main/java/zmuzik/ubike/MainActivity.kt
@@ -0,0 +1,36 @@
+package zmuzik.ubike
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var mTextMessage: TextView? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_map -> {
+                mTextMessage!!.setText(R.string.map)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_list -> {
+                mTextMessage!!.setText(R.string.list)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mTextMessage = findViewById(R.id.message) as TextView
+        val navigation = findViewById(R.id.navigation) as BottomNavigationView
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+}
