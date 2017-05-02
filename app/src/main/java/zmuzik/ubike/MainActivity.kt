360bab5b26414099a2bca108c4efbe402417567f
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-05-02 22:10:03 +0800

    kotlin android extensions

--- app/src/main/java/zmuzik/ubike/MainActivity.kt
+++ app/src/main/java/zmuzik/ubike/MainActivity.kt
@@ -4,33 +4,28 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
-    private var mTextMessage: TextView? = null
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_map -> {
-                mTextMessage!!.setText(R.string.map)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_list -> {
-                mTextMessage!!.setText(R.string.list)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mTextMessage = findViewById(R.id.message) as TextView
-        val navigation = findViewById(R.id.navigation) as BottomNavigationView
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(this)
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_map -> {
+                message.setText(R.string.map)
+                return true
+            }
+            R.id.navigation_list -> {
+                message.setText(R.string.list)
+                return true
+            }
+        }
+        return false
+    }
 }
