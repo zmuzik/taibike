b82f5808146ad6d344613e658b605421b22084bc
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-05-03 22:31:52 +0800

    viewpager with fragments

--- app/src/main/java/zmuzik/ubike/MainActivity.kt
+++ app/src/main/java/zmuzik/ubike/MainActivity.kt
@@ -2,10 +2,14 @@ package zmuzik.ubike
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -13,19 +17,27 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(this)
+        viewPager.adapter = PagesAdapter(supportFragmentManager)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.navigation_map -> {
-                message.setText(R.string.map)
-                return true
-            }
-            R.id.navigation_list -> {
-                message.setText(R.string.list)
-                return true
-            }
+            R.id.navigation_map -> viewPager.currentItem = 0
+            R.id.navigation_list -> viewPager.currentItem = 1
         }
-        return false
+        return true
+    }
+
+    private class PagesAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getCount(): Int = 2
+
+        override fun getItem(position: Int): Fragment? {
+            when (position) {
+                0 -> return SupportMapFragment.newInstance()
+                1 -> return StationsListFragment()
+            }
+            return null
+        }
     }
 }
---
+++ app/src/main/java/zmuzik/ubike/StationsListFragment.java
@@ -0,0 +1,48 @@
+package zmuzik.ubike;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import zmuzik.ubike.dummy.DummyContent;
+
+public class StationsListFragment extends Fragment {
+
+    public StationsListFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_station_list, container, false);
+
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            recyclerView.setAdapter(new StationsListAdapter(DummyContent.ITEMS));
+        }
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
